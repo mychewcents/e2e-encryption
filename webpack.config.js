7d5a3caf00458793c25a53f54d297ed81e8b4402
Akarsh Agarwal <akarsh.agrawal2010@gmail.com>
2020-07-11 15:25:14 +0530

    Adding more setup files.

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+const { NODE_ENV, FILE_NAME } = process.env;
+
+const fileName = `${FILE_NAME}${NODE_ENV === 'production' ? '.min' : ''}.js`;
+
+module.exports = {
+  mode: NODE_ENV || 'development',
+  entry: ['./src/index.js'],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    fileName,
+    libraryTarget: 'umd',
+  },
+};
