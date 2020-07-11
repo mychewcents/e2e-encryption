6049ef62759e8b7ecf7f901691dacc2f2946f62d
Akarsh Agarwal <akarsh.agrawal2010@gmail.com>
2020-07-11 15:09:51 +0530

    Adding the TODO list.

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/**
+ * Takes in the algorithm to use and the text to encrypt
+ * @param {String} algo Algorithm type
+ * @param {String} text Plain text
+ */
+
+const Encrypt = (algo, text) => text;
+
+export default Encrypt;
